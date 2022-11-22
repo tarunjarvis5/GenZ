0a99c20a16de75c90e52b965a3f33d8e81d4647f
Ravi Teja Karanam <ravitejakaranam1999@gmail.com>
2022-11-23 01:04:55 +0530

    Deployed Details Base - fix

--- PlatformApp/GenZPlatformApp/Client/ViewModels/DeployedDetailsBase.cs
+++ PlatformApp/GenZPlatformApp/Client/ViewModels/DeployedDetailsBase.cs
@@ -1,9 +1,10 @@
 ﻿using GenZPlatformApp.Data.Model;
+using Microsoft.AspNetCore.Components;
 
 namespace GenZPlatformApp.Client.ViewModels
 {
-    public class DeployedDetailsBase
-    {
+    public class DeployedDetailsBase : ComponentBase
+    { 
         public DeployFormModel DeployFormModel { get; set; } = new();
     }
 }
